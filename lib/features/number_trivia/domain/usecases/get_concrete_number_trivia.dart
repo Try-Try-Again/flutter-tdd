9971070687b0c6d7c3fc57700dcd20bccfd60669
ryan <ryan@pop-os.localdomain>
2021-09-09 10:54:25 -0700

    test pass with refactor call method

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -7,9 +7,9 @@ import '../repositories/number_trivia_repository.dart';
 class GetConcreteNumberTrivia {
 	final NumberTriviaRepository repository;
 
-	GetConcreteNumberTrivia (this.repository);
+	GetConcreteNumberTrivia(this.repository);
 
-	Future<Either<Failure, NumberTrivia>> execute({
+	Future<Either<Failure, NumberTrivia>> call({
 		@required int number
 	}) async {
 		return await repository.getConcreteNumberTrivia(number);
