67b3f2d6ad0b6bf424942379cbefc80682af73c6
ryan <ryan@pop-os.localdomain>
2021-09-09 13:25:13 -0700

    both tests pass!

---
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -0,0 +1,24 @@
+import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_architecture_tdd_course/core/usecases/usecase.dart';
+import 'package:dartz/dartz.dart';
+import 'package:clean_architecture_tdd_course/core/error/failures.dart';
+import 'package:equatable/equatable.dart';
+import '../repositories/number_trivia_repository.dart';
+
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
+	final NumberTriviaRepository repository;
+
+  GetRandomNumberTrivia(this.repository);
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) async {
+    return await repository.getRandomNumberTrivia();
+  }
+}
+
+class NoParams extends Equatable {
+  NoParams();
+
+  @override
+  List<Object> get props => [];
+}
